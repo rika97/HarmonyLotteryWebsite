a3e857921ba60fdfcc8b9606080c8860ad6c1997
Rika <15hatoyar@gmail.com>
2024-08-15 18:14:02 -0700

    modified to react

---
+++ harmonylotterywebsite/src/App.js
@@ -0,0 +1,16 @@
+// import qrcode from './assets/qr-code.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Harmony Lottery
+        <img src="./assets/qr-code.svg" alt="QR Code" width="200px"/>
+        @HarmonyLotteryBot
+      </header>
+    </div>
+  );
+}
+
+export default App;
